0473426704caa0966c5c090b96b58df0f8639740
Blake Johnson <johnsonblake1@gmail.com>
2017-10-14 19:35:07 -0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+extern crate base64;
---
+++ tests/set1.rs
@@ -0,0 +1,7 @@
+extern crate cryptopals;
+
+#[test]
+fn solve_challenge_1() {
+    assert_eq!("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+               cryptopals::hex2base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
+}
