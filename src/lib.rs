af1aebebfe1e6802b3c81d2d85c8ed5aedc6338d
Blake Johnson <johnsonblake1@gmail.com>
2017-10-14 22:05:00 -0700

    set1challenge2 solved

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate base64;
 
+use std::char;
+
 pub fn hex_to_base64(input: &str) -> String {
     let bytes = hex_to_bytes(input);
     return base64::encode(bytes.as_slice());
@@ -10,4 +12,28 @@ fn hex_to_bytes(input: &str) -> Vec<u8> {
     chars.chunks(2).map(|chunk| {
         ((chunk[0].to_digit(16).unwrap() << 4) | chunk[1].to_digit(16).unwrap()) as u8
     }).collect()
+}
+
+fn bytes_to_hex(input: Vec<u8>) -> String {
+    let mut output = String::new();
+    for chunk in input {
+        output.push(char::from_digit(u32::from(chunk) >> 4, 16).unwrap());
+        output.push(char::from_digit(u32::from(chunk) & 15, 16).unwrap());
+    }
+    return output
+}
+
+pub fn xor_hex(input: &str, key: &str) -> String {
+    if input.len() != key.len() { panic!("Buffers are of different lengths, cannot fixed xor")};
+
+    let mut output: Vec<u8> = Vec::with_capacity(input.len());
+    let input_bytes = hex_to_bytes(input);
+    let mut key_bytes = hex_to_bytes(key).into_iter();
+
+
+    for input_byte in input_bytes {
+        output.push(input_byte ^ key_bytes.next().unwrap());
+    }
+
+    return bytes_to_hex(output)
 }
--- tests/set1.rs
+++ tests/set1.rs
@@ -5,3 +5,9 @@ fn solve_challenge_1() {
     assert_eq!("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
                cryptopals::hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
 }
+
+#[test]
+fn solve_challenge_2() {
+    assert_eq!("746865206b696420646f6e277420706c6179",
+               cryptopals::xor_hex("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"));
+}
