10cf81c52b44f390f59de47d305e109ce4ad48d3
Blake Johnson <johnsonblake1@gmail.com>
2017-10-14 20:44:42 -0700

    camel

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 extern crate base64;
 
-pub fn hex2base64(input: &str) -> String {
-    let bytes = hex2bytes(input);
+pub fn hex_to_base64(input: &str) -> String {
+    let bytes = hex_to_bytes(input);
     return base64::encode(bytes.as_slice());
 }
 
-fn hex2bytes(input: &str) -> Vec<u8> {
+fn hex_to_bytes(input: &str) -> Vec<u8> {
     let chars: Vec<char> = input.chars().collect();
     chars.chunks(2).map(|chunk| {
         ((chunk[0].to_digit(16).unwrap() << 4) | chunk[1].to_digit(16).unwrap()) as u8
